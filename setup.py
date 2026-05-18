a5822be0ec715fb10dcf8bf2a1cb9ec651e9b637
swarajjoshi10-ship-it <swarajjoshi10@gmail.com>
2026-05-18 19:47:32 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='realtime_source_code_analyzer',
+    version='0.1.0',
+    author='swaraj',
+    author_email="swarajjoshi10@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
